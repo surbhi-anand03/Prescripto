d7a2d0f652566f997615bf37550a1519e863cf13
Surbhi Anand <survanand64@gmail.com>
2026-01-18 17:40:29 +0530

    Fix case-sensitive import path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Doctors from './pages/Doctors'
 import Login from './pages/Login'
 import About from './pages/About'
