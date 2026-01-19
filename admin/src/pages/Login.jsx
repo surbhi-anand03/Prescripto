b2f1c7446dc5b126af173c319d947e8fc3d6b82a
Surbhi Anand <survanand64@gmail.com>
2026-01-19 10:03:35 +0530

    Update Login.jsx

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -61,7 +61,7 @@ const Login = () => {
     <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
         <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg'>
             <a
-      href="http://localhost:5173"
+      href="https://prescripto-frontend-go3t.onrender.com"
       className="text-primary text-sm mb-2"
     >
       ← Back to Home
